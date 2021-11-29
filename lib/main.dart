8de71d9071992fb770c1626e608f19ae75d0ca51
Yash-Garg <yash.garg@outlook.in>
2021-11-29 23:34:17 +0530

    chore: remove http and init hive

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+import 'package:path_provider/path_provider.dart';
 
 import 'src/app.dart';
 
-void main() {
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  final appDocumentDir = await getApplicationDocumentsDirectory();
+  await Hive.initFlutter(appDocumentDir.path);
+
   runApp(const MainApplication());
 }
